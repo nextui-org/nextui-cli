e1ffa8115653f12ecded54fc38c93c50f0b71eeb
winches <329487092@qq.com>
2024-05-27 04:36:04 +0800

    feat: add vite init template (#67)
    * feat: add vite init template
    
    * docs: update init command

--- src/actions/init-action.ts
+++ src/actions/init-action.ts
@@ -1,4 +1,5 @@
 import type {Agent} from '@helpers/detect';
+import type {GetUnionLastValue} from '@helpers/type';
 
 import {existsSync, renameSync} from 'node:fs';
 
@@ -18,17 +19,21 @@ import {
   APP_REPO,
   PAGES_DIR,
   PAGES_NAME,
-  PAGES_REPO
+  PAGES_REPO,
+  VITE_DIR,
+  VITE_NAME,
+  VITE_REPO
 } from '../../src/constants/templates';
 
 export interface InitActionOptions {
-  template?: 'app' | 'pages';
+  template?: 'app' | 'pages' | 'vite';
   package?: 'npm' | 'yarn' | 'pnpm';
 }
 
 const templatesMap: Record<Required<InitActionOptions>['template'], string> = {
   app: APP_NAME,
-  pages: PAGES_NAME
+  pages: PAGES_NAME,
+  vite: VITE_NAME
 };
 
 export async function initAction(_projectName: string, options: InitActionOptions) {
@@ -58,6 +63,9 @@ export async function initAction(_projectName: string, options: InitActionOption
   } else if (template === 'pages') {
     await generateTemplate(PAGES_REPO);
     renameTemplate(PAGES_DIR, projectName);
+  } else if (template === 'vite') {
+    await generateTemplate(VITE_REPO);
+    renameTemplate(VITE_DIR, projectName);
   }
 
   /** ======================== Pnpm setup (optional) ======================== */
@@ -100,22 +108,40 @@ function renameTemplate(originName: string, projectName: string) {
   }
 }
 
+export type GenerateOptions<T, Last = GetUnionLastValue<T>> = [T] extends [never]
+  ? []
+  : [
+      ...GenerateOptions<Exclude<T, Last>>,
+      {
+        label: string;
+        value: Last;
+        hint: string;
+      }
+    ];
+
 async function getTableInfo(packageName?: string, projectName?: string, template?: string) {
+  const options: GenerateOptions<Exclude<InitActionOptions['template'], undefined>> = [
+    {
+      hint: 'A Next.js 14 with app directory template pre-configured with NextUI (v2) and Tailwind CSS.',
+      label: chalk.gray('App'),
+      value: 'app'
+    },
+    {
+      hint: 'A Next.js 14 with pages directory template pre-configured with NextUI (v2) and Tailwind CSS.',
+      label: chalk.gray('Pages'),
+      value: 'pages'
+    },
+    {
+      hint: 'A Vite template pre-configured with NextUI (v2) and Tailwind CSS.',
+      label: chalk.gray('Vite'),
+      value: 'vite'
+    }
+  ];
+
   template = (await selectClack({
     initialValue: template,
     message: 'Select a template (Enter to select)',
-    options: [
-      {
-        hint: 'A Next.js 14 with app directory template pre-configured with NextUI (v2) and Tailwind CSS.',
-        label: chalk.gray('App'),
-        value: 'app'
-      },
-      {
-        hint: 'A Next.js 14 with pages directory template pre-configured with NextUI (v2) and Tailwind CSS.',
-        label: chalk.gray('Pages'),
-        value: 'pages'
-      }
-    ]
+    options
   })) as string;
 
   projectName = (await textClack({

ff3be651924afe9a3ca495d5dae0eb54edc05fdd
winches <329487092@qq.com>
2024-05-27 22:24:57 +0800

    fix: add initCache in updateComponents script

--- src/scripts/update/update-components.ts
+++ src/scripts/update/update-components.ts
@@ -1,6 +1,8 @@
+import {initCache} from '../cache/cache';
 import {isGithubAction, updateComponents} from '../helpers';
 
 if (!isGithubAction) {
   // Won't run on GitHub Actions
+  initCache(true);
   updateComponents();
 }

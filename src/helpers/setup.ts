1beb52ce1b88583649aa7ae837780d708eab31eb
winches <329487092@qq.com>
2024-06-14 10:00:03 +0800

    feat: upgrade command add setup pnpm (#77)

---
+++ src/helpers/setup.ts
@@ -0,0 +1,25 @@
+
+import { existsSync, writeFileSync } from 'node:fs';
+
+import { resolver } from 'src/constants/path';
+import { pnpmRequired } from 'src/constants/required';
+
+import { checkPnpm } from './check';
+import { type Agent } from './detect';
+import { fixPnpm } from './fix';
+
+export async function setupPnpm(packageManager: Agent) {
+  if (packageManager === 'pnpm') {
+    const npmrcPath = resolver('.npmrc');
+
+    if (!existsSync(npmrcPath)) {
+      writeFileSync(resolver('.npmrc'), pnpmRequired.content, 'utf-8');
+    } else {
+      const [isCorrectPnpm] = checkPnpm(npmrcPath);
+
+      if (!isCorrectPnpm) {
+        fixPnpm(npmrcPath);
+      }
+    }
+  }
+}

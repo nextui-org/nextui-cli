d675fb977bd0201d29c00eda889498f2f223558d
winches <329487092@qq.com>
2024-12-10 09:51:06 +0800

    feat: update comment

--- src/scripts/update/update-components.ts
+++ src/scripts/update/update-components.ts
@@ -3,8 +3,9 @@ import {store} from 'src/constants/store';
 import {initCache} from '../cache/cache';
 import {isGithubAction, updateComponents} from '../helpers';
 
+// Won't run on GitHub Actions
 if (!isGithubAction) {
-  // Won't run on GitHub Actions
+  // Force to update cache
   initCache(true);
   // Update beta components
   store.beta = true;
